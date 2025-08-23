ba7b579e2a97b444b0e01a78f10cb5a82813ed87
Lokesh Hans <Lokeshbaghelhans@gmail.com>
2025-08-23 17:58:43 +0530

    added score board and start button

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("script working")
+document.addEventListener("DOMContentLoaded",()=>{
+    const gameArena = document.getElementById("game-arena");
+    const arenaSize = 600;
+    const cellSize = 20;
+    let score = 0;
+    let gameStart = false;
+    let food = {x:300 , y: 200}
+    let snake = [{x:160, y:200},{x:140, y:200}, {x:120, y:200}];
+    let dx =cellSize;
+    let dy = 0;
+
+
+    function startGame(){
+        const scoreBoard = document.createElement('div');
+        scoreBoard.id = 'score-board';
+        scoreBoard.textContent = '7';
+        document.body.insertBefore(scoreBoard, gameArena);
+
+        const startButton  = document.createElement('button');
+        startButton.textContent = 'Start Game';
+        startButton.classList.add('start-button')
+        document.body.appendChild(startButton);
+
+
+        startButton.addEventListener('click',()=>{
+            
+        })
+
+    };
+    startGame();
+});
